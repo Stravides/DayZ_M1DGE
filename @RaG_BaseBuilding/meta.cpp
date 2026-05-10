b12c040baada58901f8c3d08dc7fbb07d932a93c
Stravides <Stravides@gmail.com>
2026-05-10 15:37:30 +0100

    Added Base Building by RAG

---
+++ @RaG_BaseBuilding/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 3157695626;
+name = "RaG_BaseBuilding";
+timestamp = 5250799414792936688;
---
+++ @RaG_Core/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 3556131153;
+name = "RaG_Core";
+timestamp = 5250770048047784963;
